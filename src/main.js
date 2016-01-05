c29d103c173f0afa6403c728f89a3fe61ef8f970
Brian Mock <brian@mockbrian.com>
2016-01-05 11:46:33 -0800

    Fixes the plugin

--- src/main.js
+++ src/main.js
@@ -1,29 +1,49 @@
 var squiggle = require("squiggle-lang");
 var through = require("through");
+var chalk = require("chalk");
 
-function isSquiggle(file) {
-    return /\.sqg$/.test(file);
+// Have some cool formatting so the output is easier to skim.
+function h1(text) {
+    var underline = text.replace(/./g, "=");
+    var actual = chalk.bold.cyan(text + "\n" + underline);
+    console.error(actual);
 }
 
+function h2(text) {
+    var actual = chalk.bold.blue(text);
+    console.error(actual);
+}
+
+// The actually important part of the plugin. This is where the pretty
+// warning/error reporting happens. Oh, and it also returns the compiled code :P
 function compile(file, data, callback) {
-    // TODO: Allow disabling source maps.
-    var result = squiggle.compile(data, file, {embedSourceMaps: true});
+    // Always generate source maps.
+    // Browserify strips them unless you specify --debug.
+    var options = {
+        embedSourceMaps: true,
+        color: true
+    };
+    var result = squiggle.compile(data, file, options);
     if (result.parsed) {
-        // result has {parsed, warnings, sourceMap, code}
-        // TODO: Get nicely formatted warnings with source code context
-        result.warnings.forEach(function(w) {
-            console.error([
-                file + ":" + w.line + ":" + w.column,
-                w.message
-            ].join(" "));
-        });
+        if (result.warnings.length > 0) {
+            h1("Warnings for " + file);
+            console.error();
+            result.warnings.forEach(function(w) {
+                h2("Line " + w.line + ": " + w.data);
+                console.error(w.context + "\n");
+            });
+        }
         callback(null, result.code);
     } else {
-        // TODO: Handle parser errors gracefully
-        throw new Error("parse error at index: " + result.result.index);
+        var o = result.result.oopsy;
+        console.error("Error in file " + file + "\n");
+        console.error(o.data + "\n");
+        console.error(o.context + "\n");
+        throw new Error("parse error at line " + o.line);
     }
 }
 
+// Fantastic boilerplate code to grab the streaming file data and process it.
 function transform(file) {
     function write(buffer) {
         data += buffer;
@@ -42,12 +62,13 @@ function transform(file) {
     return stream;
 }
 
+function isSquiggle(file) {
+    return /\.sqg$/.test(file);
+}
+
+// Only transform things with the right file extension. Yawn.
 function process(file) {
-    if (!isSquiggle(file)) {
-        return through();
-    } else {
-        return transform(file);
-    }
+    return isSquiggle(file) ? transform(file) : through();
 }
 
 module.exports = process;
